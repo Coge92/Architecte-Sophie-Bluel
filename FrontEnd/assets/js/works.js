5995a53ad459b9a634aaa9642208b0a6d45ba9c8
Coge92 <mh.melting@gmail.com>
2024-02-08 12:05:07 +0100

    ajout de l'affichage avec filtres

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -1,36 +1,97 @@
 console.log("Hello Works ! ");
 
-const galleryContainer = document.querySelector(".gallery") // Recup du parent Div Class Gallery
+// Boutons filtres
+
+const works = await fetch("http://localhost:5678/api/works").then(response => response.json()) // Appel à l'API Get Works et désérialisation
+
+const categories = await fetch("http://localhost:5678/api/categories").then(response => response.json())
+
+const filtersContainer = document.querySelector(".menu-filter") // Recup du parent Div Class menu-filter
+
+const galleryContainer = document.querySelector(".gallery") // Recup du parent Div Class gallery
+
+// Boutons
+
+let btnFilters = "" // initialisation de la variable filtre qui contiendra le numero de la categorie à afficher
+
+const btnTous = document.querySelector(".menu-filter #reset") // recup du bouton reset 
+
+btnTous.addEventListener("click", () => { 
+
+    resetDisplayWorks() // au clic sur le btn reset, suppression des enfants du container "GalleryContainer"
+    generatedDomInsideGalleryContainer(works) // affichage de tous les travaux
+})
+
+console.log(btnTous);
 
 
 
-const works = await fetch("http://localhost:5678/api/works").then(response => response.json())
+for (let i = 0; i  < categories.length; i++) { // pour chaque catégorie (élement de la liste réponse provenant de l'API via la route categories)
 
-for (let i = 0 ; i < works.length ; i++ ) {
+    let newButton = document.createElement("button") // création d'un bouton pour chaque élement (catégorie)
+    newButton.innerText = `${categories[i].name}` // ajout d'un nom pour chaque bouton (nom de la categorie)
+    newButton.id = `${categories[i].id}` // ajout de l'ID de la catégorie en attribue ID 
 
-    let newImage = document.createElement("img") // Création de l'HTML Element img
-    newImage.src = `${works[i].imageUrl}` // ajout de la source de l'img
-    newImage.alt = `${works[i].title}` // ajout de l'attribut alt
+    newButton.addEventListener("click", () => {
+        btnFilters = newButton.id // recupération du numero de la categorie
+        displayWorks(btnFilters) // affichage des travaux ayant le numero de la categorie 
+        
+    })
 
-    let newFigCaption = document.createElement("figcaption") // Création de l'HTML Element Figcaption
-    newFigCaption.innerText = `${works[i].title}` // ajout du texte dans la balise figcaption
+    filtersContainer.appendChild(newButton) // attachement du bouton au parent Div Class menu-filter   
+
+}
+
+generatedDomInsideGalleryContainer(works) // Afficher tous les travaux par defaut (au chargement de la page)
 
 
-    let newFigure = document.createElement("figure") // Création de l'HTML Element figure
-    newFigure.id = `${works[i].id}`
-    newFigure.appendChild(newImage) // ajout de l'enfant img à figure
-    newFigure.appendChild(newFigCaption) // ajout de figcaption à figure
-    galleryContainer.appendChild(newFigure) // ajout de figure au parent Div Class Gallery
+// Works
+
+async function displayWorks(filterNumber) {  // fonction pour reset l'affichage des travaux et filtrer les travaux par categorie
+
+    resetDisplayWorks() // Suppression de tous les travaux
     
-    
+    const worksFiltered = works.filter (function (work) { //création de la liste filtrer par numero de categorie (works = tous les travaux) 
+        return work.categoryId == filterNumber // recupère les travaux qui ont le meme numero de catégorie que le numero du bouton cliqué
+    })
+
+    generatedDomInsideGalleryContainer(worksFiltered) // générer les enfants de la "galleryContainer" avec les travaux de la catégorie choisie
+
+
+console.log(btnFilters)
+console.log(worksFiltered);
 
 }
 
 
+async function generatedDomInsideGalleryContainer(worksList) {
+    for (let i = 0 ; i < worksList.length ; i++ ) { // Pour chaque réalisation (élément de la liste réponse provenant de l'API via la route works)
 
+        let newImage = document.createElement("img") // Création de l'HTML Element img
+        newImage.src = `${worksList[i].imageUrl}` // ajout de la source de l'img 
+        newImage.alt = `${worksList[i].title}` // ajout de l'attribut alt
+
+        let newFigCaption = document.createElement("figcaption") // Création de l'HTML Element Figcaption
+        newFigCaption.innerText = `${worksList[i].title}` // ajout du texte dans la balise figcaption
+
+
+        let newFigure = document.createElement("figure") // Création de l'HTML Element figure
+        newFigure.id = `${worksList[i].id}` // ajout d'un ID à figure correspondant à l'ID de la réalisation
+        newFigure.appendChild(newImage) // attachement de l'enfant img à figure
+        newFigure.appendChild(newFigCaption) // attachement de figcaption à figure
+        galleryContainer.appendChild(newFigure) // attachement de figure au parent Div Class Gallery
+        
+    }
+}
 
     
+function resetDisplayWorks() {
 
+    while (galleryContainer.firstChild) { // tant qu'il y a un firstchild
+    
+        galleryContainer.removeChild(galleryContainer.firstChild) // supprimer le first child -- Permet de supprimer tous les "figure" générer dans la fonction generatedDomInsideGalleryContainer
+    }
+}
 
 
 
